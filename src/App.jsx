85a429088a2eb5a154cf8caa8517d71d9f1726ec
Bikash <bikash1101@gmail.com>
2025-08-15 20:40:36 +0530

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-white text-3xl'>Redux and RTK ToDo</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
